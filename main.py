89a3bb618ec9545599ac534e9df42cb38bc23e4c
aniquekhawar <aniquekhawar2021@u.northwestern.edu>
2020-10-29 23:21:18 +0000

    Fixing GAE Deployment

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+import os.path
+from os import path
 import pickle
 from google.cloud import storage
 from flask import Flask, render_template, request
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-# accessing bucket where model and vectorizer are stored
-client = storage.Client()
-bucket = client.get_bucket('week_7_demo')
-blob1 = bucket.blob('model.pickle')
-blob2 = bucket.blob('tfidf_vect.pickle')
-blob1.download_to_filename('model_local')
-blob2.download_to_filename('tfidf_vect')
+if not path.exists('tfidf_vect') and not path.exists('model_local'):
+    # accessing bucket where model and vectorizer are stored
+    client = storage.Client()
+    bucket = client.get_bucket('week_7_demo')
+    blob1 = bucket.blob('model.pickle')
+    blob2 = bucket.blob('tfidf_vect.pickle')
+    blob1.download_to_filename('model_local')
+    blob2.download_to_filename('tfidf_vect')
 
 # loading in pickled model and vectorizer
 tfidf_vect = pickle.load(open('tfidf_vect', "rb"))
@@ -32,7 +35,8 @@ def prediction():
         sms_transformed = tfidf_vect.transform([sms])
 
         # return prediction on vectorized text
-        return model.predict(sms_transformed)[0]
+        lr_prediction = model.predict(sms_transformed)[0]
+        return render_template('prediction.html', pred = lr_prediction)
 
 if __name__ == '__main__':
-    app.run(host = "127.0.0.1", port = 8080, debug = True)
+    app.run(host = "0.0.0.0", port = 8080, debug = True)
